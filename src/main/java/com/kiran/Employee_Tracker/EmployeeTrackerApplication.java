8d89323686b69765aca87fc03357cf8b802930eb
Kiran Bhat <bhatkiran74@gmail.com>
2024-03-06 01:10:46 +0530

    created project

---
+++ src/main/java/com/kiran/Employee_Tracker/EmployeeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.Employee_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiran/Employee_Tracker/EmployeeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.Employee_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
